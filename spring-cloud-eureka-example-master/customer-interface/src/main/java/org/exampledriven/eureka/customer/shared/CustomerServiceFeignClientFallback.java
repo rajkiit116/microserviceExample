b0f702132dd2296a9e465f607aa542a4e1032dd0
Raj Mishra <rajm@deloitte.com>
2019-03-22 13:11:47 +0530

    eureka with feign

---
+++ spring-cloud-eureka-example-master/customer-interface/src/main/java/org/exampledriven/eureka/customer/shared/CustomerServiceFeignClientFallback.java
@@ -0,0 +1,12 @@
+package org.exampledriven.eureka.customer.shared;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerServiceFeignClientFallback implements CustomerServiceFeignClient {
+
+    public Customer getCustomer(int id) {
+        return new Customer(12, "Fallback", "Customer");
+    }
+
+}
